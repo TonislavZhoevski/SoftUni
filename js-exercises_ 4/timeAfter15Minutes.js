8240587fc028ed0354ea1ad1998d96f6465a1c54
Tonislav <110389906+TonislavZhoevski@users.noreply.github.com>
2025-08-30 01:45:16 +0300

    timeAfter15Minutes. Everything works just perfect!

---
+++ js-exercises_ 4/timeAfter15Minutes.js
@@ -0,0 +1,25 @@
+function timeAfter15Minutes (input) {
+  let hours = Number(input[0]);
+  let min = Number(input[1]);
+  let extraMin = 15;
+
+  // we calculate everything to minets here
+  let time = hours * 60 + min + extraMin; 
+
+  // we pre-calculate everything back to hours and minutes separetly 
+  let h = Math.floor(time / 60);
+  let m = time % 60;
+
+  if (h > 23) {
+    h = h - 24;
+  }
+  
+  // we set up a condition to either to have or haven't a following 0 (null) in front of the minutes
+  if (m >= 10) {
+    console.log(`${h}:${m}`);
+  } else {
+    console.log(`${h}:0${m}`);
+  }
+  
+}
+timeAfter15Minutes(["23","59"]);
