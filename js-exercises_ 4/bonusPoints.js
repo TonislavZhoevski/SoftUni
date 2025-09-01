ea83127022a1bf3c5c6f00acdfec944fe876835e
Tonislav Zhoevski <tonislav.zhoevski@outlook.com>
2025-09-01 14:58:40 +0300

    The comments weren't saved

--- js-exercises_ 4/bonusPoints.js
+++ js-exercises_ 4/bonusPoints.js
@@ -2,7 +2,7 @@ function bonusPoints (input) {
     let score = Number(input[0]);
     let bonus = 0.0;
 
-    // Here we deffine the 
+    // Here we deffine the conditions to scoring
     if (score <= 100 ) {
         bonus = 5;
     } else if (score > 1000) {
@@ -11,6 +11,7 @@ function bonusPoints (input) {
         bonus = score * 0.2;
     }
 
+    // Here we give one  more condition depends on that if the given number is even or if it ends on 5. For that we give extra 1 or 2 points
     if (score % 2 === 0) {
         bonus += 1;
     } else if (score % 10 === 5) {
