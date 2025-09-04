ee520c877d72dacd1824fb2c011bec5fb4c83b17
Tonislav <110389906+TonislavZhoevski@users.noreply.github.com>
2025-09-04 23:34:57 +0300

    switch cases exercises began

---
+++ js-exercises_5/switch-case.js
@@ -0,0 +1,11 @@
+function solve () {
+  let dayNumber = 1;
+
+  if (dayNumber == 1) {
+    console.log("Monday");
+  } else if (dayNumber == 2) {
+    console.log('Tuesday');
+  }
+}
+
+solve();
--- solve.js
+++ solve.js
@@ -1,4 +1,23 @@
+/* function solve () {
+  if ("caseSensitive" == "CaseSensitive") {
+    console.log("Svetlin");
+  } else {
+    console.log("Petar");
+  }
+}
+solve(); */
+
+/* function solve () {
+  console.log(123456 % 100 == 56);
+}
+solve(); */
+
 function solve () {
-  Math.floor();
+  let role = "Administrator";
+  if (role != "Administrator") {
+    console.log("No permission");
+  } else {
+    console.log("Welcome");
+  }
 }
 solve();
