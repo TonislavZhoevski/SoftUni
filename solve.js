a231d906d6f650023ff22fbe952f726711440b11
Tonislav <110389906+TonislavZhoevski@users.noreply.github.com>
2025-09-13 16:40:55 +0300

    exercises

--- solve.js
+++ solve.js
@@ -1,23 +1,28 @@
-/* function solve () {
-  if ("caseSensitive" == "CaseSensitive") {
-    console.log("Svetlin");
-  } else {
-    console.log("Petar");
-  }
-}
-solve(); */
+// function solve (input) {
+// }
+// solve(["0"]);
 
-/* function solve () {
-  console.log(123456 % 100 == 56);
-}
-solve(); */
+// let a = 50;
+// let b = 200;
+// let c = 300;
 
-function solve () {
-  let role = "Administrator";
-  if (role != "Administrator") {
-    console.log("No permission");
-  } else {
-    console.log("Welcome");
-  }
-}
-solve();
+// if ((a >= 100 && b <= 200) || (c + b >= 300 && c <= 400)) {
+//   console.log("Yes"); // Yes
+// }
+
+// if (a >= 100 && (b <= 200 || c + b >= 300) && c <= 400) {
+//   console.log("Yes"); // No output
+// }
+
+let result = "2:3:4";
+
+let input = result.split(":");
+
+console.log(input);
+
+const first = Number(input[0]);
+const second = Number(input[1]);
+const third = Number(input[2]);
+
+console.log(first + second + third);
+
