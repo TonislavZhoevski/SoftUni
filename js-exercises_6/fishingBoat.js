574cb60f2384d9de629f1c8d44c8a3f8993f97f1
Tonislav <110389906+TonislavZhoevski@users.noreply.github.com>
2025-09-22 23:54:44 +0300

    fishingBoat_repaire

--- js-exercises_6/fishingBoat.js
+++ js-exercises_6/fishingBoat.js
@@ -7,31 +7,18 @@ function fishingBoat(input) {
 
   if (season === "Spring") {
     rent = 3000;
-    if (countFisherMan <= 6) {
-      rent = rent * 0.9;
-    } else if (countFisherMan >= 7 && countFisherMan <= 11) {
-      rent = rent * 0.85;
-    } else {
-      rent = rent * 0.75;
-    }
   } else if (season === "Summer" || season === "Autumn") {
     rent = 4200;
-    if (countFisherMan <= 6) {
-      rent = rent * 0.90;
-    } else if (countFisherMan >= 7 && countFisherMan <= 11) {
-      rent = rent * 0.85;
-    } else {
-      rent = rent * 0.75;
-    }
   } else {
     rent = 2600;
-        if (countFisherMan <= 6) {
-      rent = rent * 0.90;
-    } else if (countFisherMan >= 7 && countFisherMan <= 11) {
-      rent = rent * 0.85;
-    } else {
-      rent = rent * 0.75;
-    }
+  }
+
+  if (countFisherMan <= 6) {
+    rent = rent * 0.90;
+  } else if (countFisherMan >= 7 && countFisherMan <= 11) {
+    rent = rent * 0.85;
+  } else {
+    rent = rent * 0.75;
   }
 
   if (countFisherMan % 2 === 0 && season !== "Autumn") {
@@ -41,9 +28,9 @@ function fishingBoat(input) {
   let diff = Math.abs(budget - rent);
 
   if (budget >= rent) {
-    console.log(`Yes! Yoi have ${diff.toFixed(2)} leva left.`);
+    console.log(`Yes! You have ${diff.toFixed(2)} leva left.`);
   } else {
-    console.log(`Not enough money! Yoe need ${diff.toFixed(2)} leva.`);
+    console.log(`Not enough money! You need ${diff.toFixed(2)} leva.`);
   }
 }
-fishingBoat(["3600", "Autumn", "6"]);
+fishingBoat(["2000", "Winter", "13"]);
