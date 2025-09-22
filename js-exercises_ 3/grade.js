5e070c2c3a73161872d60ab3f3ef43f8dd0b256e
Tonislav Zhoevski <tonislav.zhoevski@outlook.com>
2025-09-22 16:45:02 +0300

    grade.js

--- js-exercises_ 3/grade.js
+++ js-exercises_ 3/grade.js
@@ -1,16 +1,6 @@
-function isExcellent (input) {
-    let grade = Number(input[0]);
-
+function isExcellent (grade) {
     if (grade >= 5.50) {
-        console.log('Excellent!');
-    } else if (grade >= 4.50) {
-        console.log('Very Good!');
-    } else if (grade >= 3.50) {
-        console.log('Good!');
-    } else if (grade >= 3) {
-        console.log('Avarage!'); 
-    } else {
-        console.log('Poor!');
+        console.log("Excellent!");
     }
 }
-isExcellent(["2.88"]);
+isExcellent(6);
