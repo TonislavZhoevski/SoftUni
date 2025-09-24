c11cc8263c7e10a6a789cd0bf4d3dbc8bfb8a9af
Tonislav <110389906+TonislavZhoevski@users.noreply.github.com>
2025-09-24 18:19:28 +0300

    24.09.2025

---
+++ js-exercises_7/demo.js
@@ -0,0 +1,22 @@
+function forLoop() {
+  // let a = 4;
+
+  // console.log(a); // 4
+  // console.log(a--); // 4
+  // console.log(a); // 3
+  // console.log(--a); // 2 
+
+  
+ 
+  // console.log(a); // 4
+  // console.log(a++); // 4
+  // console.log(a); // 5
+  // console.log(++a); // 6
+  
+  for (let a = 1; a <= 10; a++) {
+    console.log(`a value: ${a}`);
+  }
+
+
+}
+forLoop();
--- solve.js
+++ solve.js
@@ -1,28 +1,8 @@
-// function solve (input) {
-// }
-// solve(["0"]);
+let role = "SoftUni";
+let password = "SoftUni";
 
-// let a = 50;
-// let b = 200;
-// let c = 300;
-
-// if ((a >= 100 && b <= 200) || (c + b >= 300 && c <= 400)) {
-//   console.log("Yes"); // Yes
-// }
-
-// if (a >= 100 && (b <= 200 || c + b >= 300) && c <= 400) {
-//   console.log("Yes"); // No output
-// }
-
-let result = "2:3:4";
-
-let input = result.split(":");
-
-console.log(input);
-
-const first = Number(input[0]);
-const second = Number(input[1]);
-const third = Number(input[2]);
-
-console.log(first + second + third);
-
+if (role === "SoftUni") {
+  if (password === "SoftUni") {
+    console.log("Welcome!");
+  }
+}
