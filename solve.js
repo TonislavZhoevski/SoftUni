8fec7aa5ea8e2d7bd75a96550cf5a890ecca80d7
Tonislav <110389906+TonislavZhoevski@users.noreply.github.com>
2025-09-28 22:24:03 +0300

    for loop, Math.min, Math.max, sum += currentNum

--- solve.js
+++ solve.js
@@ -1,8 +1,27 @@
-let role = "SoftUni";
-let password = "SoftUni";
+function solve(input) {
 
-if (role === "SoftUni") {
-  if (password === "SoftUni") {
-    console.log("Welcome!");
+  let startNum = Math.min(Number(input[0]), Number(input[1])); //200
+  let endNum = Math.max(Number(input[0]), Number(input[1])); //100
+
+  let sum = 0;
+
+  for (let currentNum = startNum; currentNum <= endNum; currentNum++) {
+    
+    if (currentNum % 9 === 0) {
+      sum += currentNum; // or sum = sum + currentNum; 
+    }
+
   }
+
+  console.log(`The sum: ${sum}`);
+
+  for (let currentNum = startNum; currentNum <= endNum; currentNum++) {
+    
+    if (currentNum % 9 === 0) {
+      console.log(currentNum);
+    }
+
+  }
+
 }
+solve(["200", "100"]);
