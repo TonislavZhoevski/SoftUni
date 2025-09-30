5f9663a645b0b3094f17e362121d5b6f467e2904
Tonislav <110389906+TonislavZhoevski@users.noreply.github.com>
2025-09-30 23:30:25 +0300

    30.09.2025 night

---
+++ while.js
@@ -0,0 +1,17 @@
+function demo() {
+  
+  for (let a = 1; a <= 5; a++) {
+    console.log(a);
+  }
+
+  console.log("------------");
+  
+  let num = 1;
+  while (num <= 5) {
+    console.log(num);
+    num++;
+  
+  }
+
+}
+demo()
