f311a0797d392338e42110f7f30b63861a79b9b9
Tonislav <110389906+TonislavZhoevski@users.noreply.github.com>
2025-10-01 21:29:52 +0300

    break operator

--- while.js
+++ while.js
@@ -1,27 +1,26 @@
-function demo(input) {
+function demo() {
   
-  let budget = Number(input[0]);
-  let index = 1; // help for the next array, couse we can increment/decrement it
-  let product = input[index]; // it stores the value of 'index' through iterating the 'input' variable(array in this case).
-
-  while (budget > 0) /* it is not 0 yet */  {
+  // for (let a = 1; a <= 10; a++) {
     
-    switch (product) {
-      case "meat": budget -= 30; break;
-      case "coffee": budget -= 10; break;
-      case "fruits": budget -= 20; break;
-      case "milk": budget -= 5; break;
-    }
-    index++;
-    product = input[index];
+  //   if (a === 7) {
+  //     break;
+  //   }
+  //   console.log(a);
 
+  // }
+
+  // console.log("-----------");
+
+  let num = 1;
+  while (num <= 10) {
+    if (num === 7) {
+      break;
+    }
+    console.log(num);
+    num++;
   }
-  console.log(`Money left: ${budget}`);
+
+  console.log("after loop");
 
 }
-demo(["100", "meat", "coffee", "fruits", "meat", "fruits", "milk"]);
-
-// meet - 30 lv/kg
-// coffee - 10 lv/kg
-// fruits - 20 lv/kg
-// milk - k lv/l
+demo();
