4276ad16e6669223e207eafd9d047136ad05b428
Tonislav Zhoevski <tonislav.zhoevski@outlook.com>
2025-10-02 14:23:10 +0300

    charSequence

---
+++ js-exercises_7/charSequence.js
@@ -0,0 +1,13 @@
+function charSequence (text) {
+    
+    /* for (let i = 0; i < text.length; i++) {
+        let letter = text[i];
+        console.log(letter);
+    } */
+
+    for (let i = 0; i <= text.length - 1; i++) {
+        console.log(text[i]);
+    }
+    
+}
+charSequence("softuni");
