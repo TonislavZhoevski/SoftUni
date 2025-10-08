1d1c6676ee1143a26ddcc49f4a770b941f5589ef
Tonislav <110389906+TonislavZhoevski@users.noreply.github.com>
2025-10-08 23:10:24 +0300

    8.10.2025 - night

--- 6-complex-validationsEXM/fishingBoat.js
+++ 6-complex-validationsEXM/fishingBoat.js
@@ -1,6 +1,6 @@
 function fishingBoat(budget, season, countFisherMan) {
-  rent = 0;
-
+  let rent = 0;
+  // -------------
   if (season === "Spring") {
     rent = 3000;
   } else if (season === "Summer" || season === "Autumn") {
@@ -9,6 +9,7 @@ function fishingBoat(budget, season, countFisherMan) {
     rent = 2600;
   }
 
+  // --------------
   if (countFisherMan <= 6) {
     rent = rent * 0.90;
   } else if (countFisherMan >= 7 && countFisherMan <= 11) {
@@ -17,16 +18,17 @@ function fishingBoat(budget, season, countFisherMan) {
     rent = rent * 0.75;
   }
 
+  //-----------------
   if (countFisherMan % 2 === 0 && season !== "Autumn") {
     rent = rent * 0.95;
   }
 
-  let diff = Math.abs(budget - rent);
+  let diff = Math.abs(budget - rent).toFixed(2);
 
   if (budget >= rent) {
-    console.log(`Yes! You have ${diff.toFixed(2)} leva left.`);
+    console.log(`Yes! You have ${diff} leva left.`);
   } else {
-    console.log(`Not enough money! You need ${diff.toFixed(2)} leva.`);
+    console.log(`Not enough money! You need ${diffr} leva.`);
   }
 }
 fishingBoat(2000, "Winter", 13);
