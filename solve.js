6eda6ae3836948461a6631b747a7fd5d1dadcfa9
Tonislav <110389906+TonislavZhoevski@users.noreply.github.com>
2025-10-20 22:50:29 +0300

    while training 1

--- solve.js
+++ solve.js
@@ -1,32 +1,13 @@
-function solve(input) {
+function solve() {
 
-  let tournaments = Number(input[0]);
-  let startingPoints = Number(input[1]);
-  let winPoints = 0;
-  let wins = 0;
-
-  for (let i = 2; i < input.length; i++) {
-
-    let score = input[i];
-
-    switch (score) {
-      case "W": winPoints += 2000;
-        wins++;
-        break;
-      case "F": winPoints += 1200;
-        break;
-      case "SF": winPoints += 720;
-        break;
-    }
+  let i = 0;
+  let indexStr = 0;
+  while (i <= 5) {
+    indexStr++;
+    indexStr = String(indexStr)
+    console.log(`${indexStr} SoftUni`);
+    i++;
   }
 
-  let avgPoints = winPoints / tournaments;
-  let percentWonTournaments = (wins / tournaments) * 100;
-  let finalPoints = startingPoints + winPoints;
-
-  console.log(`Final points: ${finalPoints}`);
-  console.log(`Avarage points: ${Math.floor(avgPoints)}`);
-  console.log(`${percentWonTournaments.toFixed(2)}%`);
-
 }
-solve(["5", "1400", "F", "SF", "W", "W", "SF"]);
+solve();
