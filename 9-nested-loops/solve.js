44d99dc1411d88fe10d4b2e0644aad7985a73fbe
Tonislav <110389906+TonislavZhoevski@users.noreply.github.com>
2025-10-21 00:15:50 +0300

    magicNumber

--- 9-nested-loops/solve.js
+++ 9-nested-loops/solve.js
@@ -1,9 +1,31 @@
-function nestedLoops() {
+function nestedLoops(input) {
   
-  for (let first = 0; first <= 10; first++) {
-    for (let second = 1; second <= 10; second++) {
-      console.log(`${first} * ${second} = ${first * second}`);
+  let startInterval = Number(input[0]);
+  let endInterval = Number(input[1]);
+  let magicNumber = Number(input[2]);
+  let combinationsCounter = 0;
+  let isFoundValidCombination = false;
+
+  for (let a = startInterval; a <= endInterval; a++) {
+    for (let b = startInterval; b <= endInterval; b++) {
+      
+      combinationsCounter++;
+      let sum = a + b;
+
+      if (sum === magicNumber) {
+        isFoundValidCombination = true;
+        console.log(`Combination N:${combinationsCounter} (${a} + ${b} = ${sum})`);
+        break;
+      }
+    }
+    if (isFoundValidCombination) {
+      break;
     }
   }
+  
+  if (isFoundValidCombination !== true) {
+    console.log(`4 combinations - neither equals ${magicNumber}`);
+  }
+
 }
-nestedLoops();
+nestedLoops(["23", "24", "20"]);
