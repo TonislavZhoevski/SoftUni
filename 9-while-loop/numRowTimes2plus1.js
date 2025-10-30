da010ce0467cd83d49e4f54f41b2cc009d2ae41f
Tonislav Zhoevski <tonislav.zhoevski@outlook.com>
2025-10-30 08:55:23 +0200

    numRowTimes2plus1

---
+++ 9-while-loop/numRowTimes2plus1.js
@@ -0,0 +1,10 @@
+function numRowTimes2plus1 (n) {
+    
+    let index = 1;
+
+    while (index <= n) {
+        console.log(index);
+        index = (index * 2) + 1;
+    }
+}
+numRowTimes2plus1(31);
