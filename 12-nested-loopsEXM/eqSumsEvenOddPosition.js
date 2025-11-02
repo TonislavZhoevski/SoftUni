97172c9480256bfc9a731fd0e44a53cfdaadf1bb
Tonislav <110389906+TonislavZhoevski@users.noreply.github.com>
2025-11-02 17:04:03 +0200

    eqSumsEvenOddPosition.js

---
+++ 12-nested-loopsEXM/eqSumsEvenOddPosition.js
@@ -0,0 +1,25 @@
+function eqSumsEvenOddPosition(input) {
+  let firstNum = Number(input[0]);
+  let secondNum = Number(input[1]);
+  let printLine = "";
+
+  for (let i = firstNum; i <= secondNum; i++) {
+    let currentNum = "" + i;
+    let oddSum = 0;
+    let evenSum = 0;
+    for (let j = 0; j < currentNum.length; j++) {
+      let currentDigit = Number(currentNum.charAt(j));
+      if (j % 2 === 0) {
+        evenSum += currentDigit;
+      } else {
+        oddSum += currentDigit;
+      }
+    }
+    if (oddSum === evenSum) {
+      printLine += `${i} `;
+    }
+  }
+  console.log(printLine);
+}
+// eqSumsEvenOddPosition(["100000", "100050"]);
+eqSumsEvenOddPosition(["123456","124000"]);
