186c3161aa136d26233b7127ca13c233f58324b0
Tonislav <110389906+TonislavZhoevski@users.noreply.github.com>
2025-11-02 15:18:23 +0200

    oldLib

--- 10-while-loopEXM/oldLib.js
+++ 10-while-loopEXM/oldLib.js
@@ -1,3 +1,40 @@
-function oldLib() {
-  
+function oldLib(arr) {
+  const favoriteBook = arr[0];
+
+  let index = 1;
+  let nextBook = arr[index];
+
+  while (nextBook !== "No More Books") {
+    if (nextBook === favoriteBook) {
+      console.log(`You checked ${index - 1} books and found it.`);
+      break;
+    }
+    index++;
+    nextBook = arr[index];
+  }
+
+  if (nextBook === "No More Books") {
+    console.log("The book you search is not here!");
+    console.log(`You checked ${index - 1} books.`);
+  }
 }
+oldLib([
+  "The Spot",
+  "Hunger Games",
+  "Harry Potter",
+  "Torronto",
+  "Spotify",
+  "No More Books"
+]);
+
+/* "The Spot",
+"Hunger Games",
+"Harry Potter",
+"Torronto",
+"Spotify",
+"No More Books" */
+
+/* "Troy",
+"Stronger",
+"Life Style",
+"Troy" */
