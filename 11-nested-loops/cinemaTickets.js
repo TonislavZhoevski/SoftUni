c842f8b3c0d2f452d83f0738cc56475e0d54f068
Tonislav <110389906+TonislavZhoevski@users.noreply.github.com>
2025-12-11 20:46:23 +0200

    cinemaTickets

--- 11-nested-loops/cinemaTickets.js
+++ 11-nested-loops/cinemaTickets.js
@@ -1,47 +1,55 @@
 function cinemaTickets(input) {
-  
   let index = 0;
-  let row = input[index];
-  let standardTickets = 0;
-  let kidTickets = 0;
-  let studentTickets = 0;
 
-  while (row !== "Finish") {
+  let command = input[index];
+  index++;
+  let studentTicketCount = 0;
+  let standardTicketCount = 0;
+  let kidTicketCount = 0;
 
-    let filmName = row;
-    index++;
+  while (command !== "Finish") {
+    let name = command;
     let freeSpace = Number(input[index]);
-    
-    let ticketsPerFilm = 0; // изнесено извън вътрешния while
     index++;
-    row = input[index]; // подготвяме първия тип билет
 
-    while (row !== "End") {
-      let ticketType = row;
+    let ticketCount = 0; // it will be created here, because it is necessarry to have it again and again by every while(ticketType !== "End")-loop spinning with the new data for free space in percentage
 
+    let ticketType = input[index];
+    index++;
+
+    while (ticketType !== "End") {
       switch (ticketType) {
-        case "standard": standardTickets++; break;
-        case "kid": kidTickets++; break;
-        case "student": studentTickets++; break;
+        case "student": studentTicketCount++; break;
+        case "standard": standardTicketCount++; break;
+        case "kid": kidTicketCount++; break;
       }
-
-      ticketsPerFilm++;
-      console.log(ticketsPerFilm);
-      
+      ticketCount++;
+      if (ticketCount === freeSpace) {
+        break;
+      }
+      ticketType = input[index];
       index++;
-      row = input[index];// 🔹 обновяваме row, за да излезем, когато стане "End"
     }
 
-    console.log(filmName);
-    console.log(freeSpace);
-
-    index++; // минаваме на следващия филм
-    row = input[index]; // 🔹 и отново обновяваме row за външния while
+    let soldTickets = ticketCount / freeSpace * 100;
+    console.log(`${name} - ${soldTickets.toFixed(2)}% full`);
+    command = input[index];
+    index++;
   }
-  
 
+
+  let totalTicket = studentTicketCount + standardTicketCount + kidTicketCount;
+  let student = studentTicketCount / totalTicket * 100;
+  let standard = standardTicketCount / totalTicket * 100;
+  let kid = kidTicketCount / totalTicket * 100;
+  console.log(`Total tickets: ${totalTicket}`);
+  console.log(`${student.toFixed(2)}% student tickets.`);
+  console.log(`${standard.toFixed(2)}% standard tickets.`);
+  console.log(`${kid.toFixed(2)}% kid tickets.`);
+  
 }
-cinemaTickets(["Taxi",
+cinemaTickets([
+  "Taxi",
   "10",
   "standard",
   "kid",
@@ -58,4 +66,34 @@ cinemaTickets(["Taxi",
   "student",
   "student",
   "student",
-  "Finish"]);
+  "Finish"
+]);
+
+/* cinemaTickets([
+  "The Matrix",
+  "20",
+  "student",
+  "standard",
+  "kid",
+  "kid",
+  "student",
+  "student",
+  "standard",
+  "student",
+  "End",
+  "The Green Mile",
+  "17",
+  "student",
+  "standard",
+  "standard",
+  "student",
+  "standard",
+  "student",
+  "End",
+  "Amadeus",
+  "3",
+  "standard",
+  "standard",
+  "standard",
+  "Finish"
+]); */
